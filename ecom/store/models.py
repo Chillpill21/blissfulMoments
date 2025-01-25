ea22b92dfaa8b0e41e1d687e5997b05e1608cae9
Chillpill9797 <adityagoswami193@gmail.com>
2025-01-26 00:08:55 +0530

    add media to urls.py and create models

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Models):
+    pass
+
+
+class Customer(models.Models):
+    pass
+
+class Product(models.Models):
+    pass
+
+class Order(models.Models):
+    pass
