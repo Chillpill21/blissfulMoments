a684d1872cbe51d9e458f5adb94ab2642249211c
Chillpill9797 <adityagoswami193@gmail.com>
2025-01-26 22:05:13 +0530

    create other models, install Pillow for images and make model migrations

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,19 +1,42 @@
 from django.db import models
 import datetime
+# 
 
-
-class Category(models.Models):
+class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
-class Customer(models.Models):
-    pass
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
 
-class Product(models.Models):
-    pass
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
-class Order(models.Models):
-    pass
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    catregory = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
