018d2205c98d9bdbf4106040874a5dc2bb90aaf5
Chillpill9797 <adityagoswami193@gmail.com>
2025-01-28 19:32:09 +0530

    adding product images

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -3,4 +3,5 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html', {'products':products})
+    return render(request, 'index.html', {'products':products
+    })
