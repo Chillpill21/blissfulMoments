76895bdd2ceaa56ccba18b99a85b06b67a2a1ac6
Chillpill9797 <adityagoswami193@gmail.com>
2025-01-29 23:59:05 +0530

    create login/logout url, create login page, login/logout views, add link to navbar

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
     products = Product.objects.all()
@@ -8,3 +10,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    pass
