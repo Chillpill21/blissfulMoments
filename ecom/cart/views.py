96709ca1c84e3aa84de6988db0e3c0f4f8597c5a
Chillpill9797 <adityagoswami193@gmail.com>
2025-02-18 22:36:25 +0530

    add message to cart view, to  product page and add page reload javascript

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
+
 
 def cart_summary(request):
     # Get the cart
@@ -29,7 +31,7 @@ def cart_add(request):
         # Return response
         # response = JsonResponse({'Product Name': product.name})
         response = JsonResponse({'qty': cart_quantity})
-
+        messages.success(request, ("Product added to Cart!!"))
         return response
 
 def cart_delete(request):
