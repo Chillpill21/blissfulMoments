ad59924de2dd42d2e38e52990e82934cc5086f87
Chillpill9797 <adityagoswami193@gmail.com>
2025-02-26 23:32:44 +0530

    create navbar link dropdown, create profile form and forms.py minor update

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     phone = models.CharField(max_length=20, blank=True)
     address1 = models.CharField(max_length=20, blank=True)
     address2 = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=20, blank=True)
     state = models.CharField(max_length=20, blank=True)
     zipcode = models.CharField(max_length=20, blank=True)
     country = models.CharField(max_length=20, blank=True)
