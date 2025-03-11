ce6654bd7cd1259994a33e454faa2990eab4c084
Chillpill9797 <adityagoswami193@gmail.com>
2025-03-11 22:35:40 +0530

    shipping form update, modify update info page, update store views.py and add shipping label

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 from django import forms
 from django.db.models import Q
 import json
@@ -28,13 +30,18 @@ def search(request):
 def update_info(request):
     if request.user.is_authenticated:
         current_user = Profile.objects.get(user__id = request.user.id)
+        # Get current user's Shipping Info
+        shipping_user = ShippingAddress.objects.get(id=request.user.id)
+        # Get original user form
         form = UserInfoForm(request.POST or None, instance=current_user)
+        # Get User's Shipping form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
 
         if form.is_valid():
             form.save()
             messages.success(request, "Your Info has been Updated!!")
             return redirect('index')
-        return render(request, "update_info.html", {"form": form})
+        return render(request, "update_info.html", {"form": form, 'shipping_form':shipping_form})
     else:
         messages.success(request, "You must be logged in first!!")
         return redirect('index')
