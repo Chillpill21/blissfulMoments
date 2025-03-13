eb4dc05d4f916ed9bfb2f179cc71f638ae2b7f9a
Chillpill9797 <adityagoswami193@gmail.com>
2025-03-13 22:22:36 +0530

    create order item model, make migrations, migrate and add models to admin area

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -34,3 +34,14 @@ class Order(models.Model):
         return f"Order - {str(self.id)}"
 
 # Create Order Items Model
+class OrderItem(models.Model):
+    # FOREIGN KEYS
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"Order Item - {str(self.id)}"
