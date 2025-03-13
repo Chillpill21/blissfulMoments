cc73ec8ed7537a55be5764df2c03c50cfaa08e98
Chillpill9797 <adityagoswami193@gmail.com>
2025-03-13 22:00:11 +0530

    import product model, order models and create order models

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 # Create your models here.
 class ShippingAddress(models.Model):
@@ -18,3 +19,18 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
+    
+# Create order Model
+class Order(models.Model):
+    # FOREIGN KEY
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order - {str(self.id)}"
+
+# Create Order Items Model
