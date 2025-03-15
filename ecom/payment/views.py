c48a8cd4e92b1d695d7b7a4767093ec169675fa1
Chillpill9797 <adityagoswami193@gmail.com>
2025-03-15 16:53:18 +0530

    Get quantities, add summary total, update summary button and shipping address box

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -7,7 +7,7 @@ def checkout(request):
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.cart_total()
-    return render(request, 'payment/checkout.htm', {"cart_products":cart_products, "quantities": quantities, "totals":totals})
+    return render(request, 'payment/checkout.html', {"cart_products":cart_products, "quantities": quantities, "totals":totals})
     
 
 def payment_success(request):
