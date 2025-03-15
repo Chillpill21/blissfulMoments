bd77d4d7d4340f6e722fe4cf9e57c6abcda70ffd
Chillpill9797 <adityagoswami193@gmail.com>
2025-03-15 17:17:02 +0530

    Add continue button, check for logged in, get shipping info and add shipping logged out

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from cart.cart import Cart
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 
 # Create your views here.
 def checkout(request):
@@ -7,7 +9,15 @@ def checkout(request):
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.cart_total()
-    return render(request, 'payment/checkout.html', {"cart_products":cart_products, "quantities": quantities, "totals":totals})
+    if request.user.is_authenticated:
+        # Checkout as logged in user
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html', {"cart_products":cart_products, "quantities": quantities, "totals":totals, "shipping_form":shipping_form})
+    else:
+        # Checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
+        return render(request, 'payment/checkout.html', {"cart_products":cart_products, "quantities": quantities, "totals":totals, "shipping_form":shipping_form})
     
 
 def payment_success(request):
