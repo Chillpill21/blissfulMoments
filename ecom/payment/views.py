6efe52cb8ea9249b0baace12c9487285c5689dfc
Chillpill9797 <adityagoswami193@gmail.com>
2025-03-25 23:12:49 +0530

    Save order item, test it out and Replicate for Unlogged

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -50,6 +50,7 @@ def process_order(request):
                     if int(key) == product.id:
                         # Create order item
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
             messages.success(request, "Order Placed!")
             return redirect('index')
         else:
@@ -57,6 +58,26 @@ def process_order(request):
             # Create Order
             create_order = Order(full_name=full_name, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
+            # Add order items
+            # Get the order ID
+            order_id = create_order.pk
+            # Get product info
+            for product in cart_products():
+                # Get product ID
+                product_id = product.id
+                # Get product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                # Get quantity
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+
             messages.success(request, "Order Placed!")
             return redirect('index')
 
