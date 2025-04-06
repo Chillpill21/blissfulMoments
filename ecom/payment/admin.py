6cbf526baf156c308984ade841035d08c551cfe0
Chillpill9797 <adityagoswami193@gmail.com>
2025-04-06 12:52:12 +0530

    Import User To admin.py, Create OrderItem Inline, Extend Order Model and Unregister/Register Models.

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 
 # Register the model on the admin section thing
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+# Create an OrderItem Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+# Extend our Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderItemInline]
+
+# Unregister Order Model
+admin.site.unregister(Order)
+
+# Re-Register our Order AND OrderAdmin
+admin.site.register(Order, OrderAdmin)
