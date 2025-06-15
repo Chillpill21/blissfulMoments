82fee4a9886cf0cb5f8d2dfb0d900418583245c5
Chillpill9797 <adityagoswami193@gmail.com>
2025-06-15 22:25:57 +0530

    make debug false

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = []
